d29d08e05729a6159e0b12683cf3ae85e1cceea9
vanila-hub <149675635+Vanila-Hub@users.noreply.github.com>
2024-01-25 11:33:50 +0100

    falta testear

--- TEMA_2/src/sql_inicio/Main_Select_2_delete_SQL.java
+++ TEMA_2/src/sql_inicio/Main_Select_2_delete_SQL.java
@@ -2,6 +2,7 @@ package sql_inicio;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,16 +13,106 @@ import interfaces.ConexionEthernet;
 
 public class Main_Select_2_delete_SQL {
 
+	private static ArrayList<Tarea> tareas = new ArrayList<Tarea>();
+	private static Tarea tarea = new Tarea();
+	private static Scanner scan = new Scanner(System.in);
+	private static String BBDD = "gestor_tarea";
+	private static String Usuario = "root";
+	private static String contraseña = "";
+	private static String host = "localhost";
 	public static void main(String[] args) {
-		ArrayList<Tarea> tareas = new ArrayList<Tarea>();
-		Tarea tarea = new Tarea();
-		Scanner scan = new Scanner(System.in);
-		String BBDD = "gestor_tarea";
-		String Usuario = "root";
-		String contraseña = "";
-		String host = "localhost";
 		
+		create_tarea();
+		deleteTarea();
+		updateTarea();
+		readBBDD_Tarea();
 		
+	}
+
+	public static void readBBDD_Tarea() {
+		//pedir el ID
+		//leer el ID
+		String query = "SELECT * FROM tareas";
+		//cargar la clase de la libreria
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("No se ha podido cargar la clase SQl java");
+		}
+		//crear la conexion
+		try {
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + "/" + BBDD,Usuario,contraseña);
+			//crear el statement
+			Statement st = conexion.createStatement();
+			//ejecutar la consulta y recibir eñ resultao
+			//Recorrer y pintar en pantalla
+			
+			ResultSet resultado2 = st.executeQuery("SELECT * FROM tareas");
+			//Recorrer y pintar en pantalla
+			while (resultado2.next()) {
+				tarea = new Tarea();
+				tarea.setId(resultado2.getInt("id"));
+				tarea.setTitulo(resultado2.getString("titulo"));
+				tarea.setDescripcion(resultado2.getString("descripcion"));
+				tareas.add(tarea);
+			}
+			for (Tarea tareaa : tareas) {
+				System.out.println(tareaa.toString());
+			}
+		} catch (SQLException e) {
+			System.out.println("NO se ha podido establecer la conexion a la base de Datos");
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void updateTarea() {
+
+		//pedir el ID
+		System.out.println("Introduzca su ID");
+		int id = Integer.parseInt(scan.nextLine());
+		String descripcion = "NUeva descripcon";
+		String titulo = "Nuevo Titulo";
+		//leer el ID
+		String query = "UPDATE tareas SET titulo = ?, descripcion = ? WHERE tareas.id = ?";
+		//cargar la clase de la libreria
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("No se ha podido cargar la clase SQl java");
+		}
+		//crear la conexion
+		try {
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + "/" + BBDD,Usuario,contraseña);
+			//crear el statement
+			PreparedStatement prst = conexion.prepareStatement(query);
+			prst.setInt(12, id);
+			prst.setString(12,descripcion);
+			prst.setString(12, titulo);
+			//ejecutar la consulta y recibir eñ resultao
+			System.out.println("Update realizado");
+			ResultSet resultado2 = prst.executeQuery("SELECT * FROM tareas");
+			//Recorrer y pintar en pantalla
+			while (resultado2.next()) {
+				tarea = new Tarea();
+				tarea.setId(resultado2.getInt("id"));
+				tarea.setTitulo(resultado2.getString("titulo"));
+				tarea.setDescripcion(resultado2.getString("descripcion"));
+				tareas.add(tarea);
+			}
+			for (Tarea tareaa : tareas) {
+				System.out.println(tareaa.toString());
+			}
+		} catch (SQLException e) {
+			System.out.println("NO se ha podido establecer la conexion a la base de Datos");
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteTarea() {
+
 		//pedir el ID
 		System.out.println("Introduzca su ID");
 		int id = Integer.parseInt(scan.nextLine());
@@ -64,5 +155,48 @@ public class Main_Select_2_delete_SQL {
 			System.out.println("NO se ha podido establecer la conexion a la base de Datos");
 			e.printStackTrace();
 		}
+	}
+
+	public static void create_tarea() {
+		//pedir el ID
+				Tarea tarea = new Tarea();
+				tarea.setId(1);
+				tarea.setDescripcion("otra descripcion");
+				tarea.setTitulo("otro titulo");
+				//leer el ID
+				String create_tarea = "INSERT INTO tareas";
+				//cargar la clase de la libreria
+				try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+					System.out.println("No se ha podido cargar la clase SQl java");
+				}
+				//crear la conexion
+				try {
+					Connection conexion = DriverManager.getConnection("jdbc:mysql://" + host + "/" + BBDD,Usuario,contraseña);
+					//crear el statement
+					Statement st = conexion.createStatement();
+					String checkId = "SELECT * FROM tareas";
+					//ejecutar la consulta y recibir eñ resultao
+					boolean resultado = st.execute(checkId );
+					
+					ResultSet resultado2 = st.executeQuery("SELECT * FROM tareas");
+					//Recorrer y pintar en pantalla
+					while (resultado2.next()) {
+						tarea = new Tarea();
+						tarea.setId(resultado2.getInt("id"));
+						tarea.setTitulo(resultado2.getString("titulo"));
+						tarea.setDescripcion(resultado2.getString("descripcion"));
+						tareas.add(tarea);
+					}
+					for (Tarea tareaa : tareas) {
+						System.out.println(tareaa.toString());
+					}
+				} catch (SQLException e) {
+					System.out.println("NO se ha podido establecer la conexion a la base de Datos");
+					e.printStackTrace();
+				}
+		
 	}
 }
